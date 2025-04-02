2a8d5788f06f5e32cdc38147ca93dee706af62b5
aswinbijukumar <aswinbijukumar2026@mca.ajce.in>
2025-04-02 20:13:17 +0530

    all done

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://todolist-one-swart.vercel.app', 
+  'https://abtodolist.vercel.app', 
   'https://todolistab.vercel.app',
   
 ];
